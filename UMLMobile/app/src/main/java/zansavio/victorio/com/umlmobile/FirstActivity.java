2396b3c37b2dfd0270615b01350b27bd7b4456ac
VictorioDev <victorio10.0@hotmail.com>
2018-06-30 12:24:14 -0300

    Modificações
    Modificações

---
+++ UMLMobile/app/src/main/java/zansavio/victorio/com/umlmobile/FirstActivity.java
@@ -0,0 +1,20 @@
+package zansavio.victorio.com.umlmobile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+    }
+
+
+}
