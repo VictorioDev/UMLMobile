0db0972d075a2ae77ccf337b9d9f57e8ae530320
Victorio Zansavio <victorio10.0@hotmail.com>
2018-06-30 13:57:41 -0300

    Implementação do scaner
    Implementação do scaner

--- UMLMobile/app/src/main/java/zansavio/victorio/com/umlmobile/FirstActivity.java
+++ UMLMobile/app/src/main/java/zansavio/victorio/com/umlmobile/FirstActivity.java
@@ -1,20 +1,57 @@
 package zansavio.victorio.com.umlmobile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FirstActivity extends AppCompatActivity {
+    private final String TAG = "App";
+    private Button btnScanear;
+    private TextView tvUrl;
+
+    private int REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        btnScanear = (Button) findViewById(R.id.btnScanear);
+        tvUrl = (TextView) findViewById(R.id.txtURL);
 
+        final Context ctx = this;
+        btnScanear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(ctx, ScanActivity.class);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQUEST_CODE){
+                Bundle b = data.getExtras();
+                if(b != null){
+                    String url = data.getStringExtra("URL");
+                    tvUrl.setText("URL: " + url);
+                }
+
+            }else {
+                Log.v(TAG, "Outro código");
+            }
+        }else {
+            Log.v(TAG, "Result Not ok");
+        }
+    }
 }
---
+++ UMLMobile/app/src/main/java/zansavio/victorio/com/umlmobile/ScanActivity.java
@@ -0,0 +1,62 @@
+package zansavio.victorio.com.umlmobile;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class ScanActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler{
+    private static final String TAG = "App";
+    private static final int ZBAR_CAMERA_PERMISSION = 1;
+    private ZBarScannerView mScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, ZBAR_CAMERA_PERMISSION);
+
+        }else {
+            mScannerView = new ZBarScannerView(this);
+            setContentView(mScannerView);
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v(TAG, rawResult.getContents()); // Prints scan results
+        Log.v(TAG, rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+
+        Intent i = new Intent();
+        i.putExtra("URL", rawResult.getContents());
+        setResult(RESULT_OK, i);
+        finish();
+
+    }
+}
